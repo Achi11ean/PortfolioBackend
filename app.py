f3da35f1da12d64cb4e91d8cd93c1ac3eac6b3cf
Jonathen Whitford <paredes.jonathen@yahoo.com>
2025-02-05 17:03:07 -0500

    .com

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Initialize Flask app
 # Initialize Flask app
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=os.getenv("CORS_ORIGINS", "*").split(","))
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": os.getenv("CORS_ORIGINS", "*").split(",")}})
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
